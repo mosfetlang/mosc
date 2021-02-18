cefa33943866dbb16cc21b385374fcde71eae6cc
Julio Treviño Páez <julio.tpaez@gmail.com>
2021-02-18 11:46:11 +0100

    [Parser] Add ParserNode trait to encapsulate common methods

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,3 +1,20 @@
+use std::sync::Arc;
+
+use crate::io::Span;
+
 pub mod errors;
 pub mod io;
 pub mod parsers;
+
+/// A trait that is implemented across all nodes belonging to the parser.
+pub trait ParserNode {
+    // GETTERS ----------------------------------------------------------------
+
+    /// The `Span` that bounds the node.
+    fn span(&self) -> &Arc<Span>;
+
+    /// The whole content of the node.
+    fn content(&self) -> &str {
+        self.span().content()
+    }
+}
